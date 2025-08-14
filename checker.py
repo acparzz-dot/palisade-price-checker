b73e52904feeabbd906632aa179ea256a6303cce
akpar <a.baimaganbetov@uzum.com>
2025-08-14 13:33:36 +0500

    init 3

--- checker.py
+++ checker.py
@@ -8,7 +8,6 @@ import os
 PDF_URL = "https://www.hyundai.com/content/dam/hyundai/kz/ru/files/prices/palisade-2025_ru.pdf"
 TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-LAST_PRICE_FILE = "last_price.txt"
 
 bot = telegram.Bot(token=TOKEN)
 
@@ -22,22 +21,10 @@ def fetch_price():
 
 def main():
     price = fetch_price()
-
-    old_price = None
-    if os.path.exists(LAST_PRICE_FILE):
-        with open(LAST_PRICE_FILE, "r", encoding="utf-8") as f:
-            old_price = f.read().strip()
-
     if price:
-        if price != old_price:
-            bot.send_message(CHAT_ID, f"Новая акционная цена Palisade 2025: {price}")
-            with open(LAST_PRICE_FILE, "w", encoding="utf-8") as f:
-                f.write(price)
+        bot.send_message(CHAT_ID, f"Акционная цена Palisade 2025: {price}")
     else:
-        if old_price != "NO_PRICE":
-            bot.send_message(CHAT_ID, "⚠ Акционная цена на Palisade 2025 больше не указана в прайс-листе.")
-            with open(LAST_PRICE_FILE, "w", encoding="utf-8") as f:
-                f.write("NO_PRICE")
+        bot.send_message(CHAT_ID, "⚠ Акционная цена на Palisade 2025 не указана в прайс-листе.")
 
 if __name__ == "__main__":
     main()
